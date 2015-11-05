ced588f799aa6a05582dcad36dbdfe9ed45409c3
Daniel Nicodemo <dan.nicodemo@gmail.com>
2015-11-05 12:01:24 -0500

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,24 @@ Rails.application.routes.draw do
 
 root "users#welcome"
 
+#REGISTRATION CONTROLLER
+post "/signup", to: "registrations#create"
+delete "/signup", to: "registrations#destroy"
+post "/login", to: "registrations#login"
 
-  ## Deck routes
-  get "/index" , to: "deck#index"
-  get "/decks/:id/cards" , to: "deck#retrieve"
 
-  post "/decks" , to: "deck#create"
-  post "/decks/:id/cards" , to: "card#create"
-
-  delete "/decks" , to: "deck#delete"
-  delete "/decks/:id/cards/" , to: "card#delete"
+#DECK AND CARD ROUTES
+get "/index" , to: "deck#index"
+get "/decks/:id/cards" , to: "deck#retrieve"
+post "/decks" , to: "deck#create"
+post "/decks/:id/cards" , to: "card#create"
+delete "/decks" , to: "deck#delete"
+delete "/decks/:id/cards/" , to: "card#delete"
 
 
 #USER CONTROLLERS
-post "/users/new", to: "users#create"
-get "/users/:id", to: "users#info" as: user
-post "/users/new", to: "users#new"
+get "/users/:id", to: "users#info", as: user
 get "/users/:id", to: "users#info"
-
 get "/users/:id/profile", to: "users#profile", as: profile
 put "/users/:id/profile", to: "users#profedit"
 post "/users/:id/profile", to: "users#profupdate"
